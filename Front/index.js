84a2c186397e7678aa6a8c2a1001ae330230d44b
Eslamzaid <113968253+Eslamzaid@users.noreply.github.com>
2023-07-08 13:52:45 +0300

    Shoppy

--- Front/index.js
+++ Front/index.js
@@ -1,19 +1,9 @@
 const h1 = document.querySelector("#LogName");
-const cr = document.querySelector(".crazy");
 
-let counter = 1;
 function makeItPretty(h) {
   let ran = Math.floor(Math.random() * 255);
   let ran2 = Math.floor(Math.random() * 255);
   let ran3 = Math.floor(Math.random() * 255);
-
-  if (parseInt(cr.style.left, 10) == 800) {
-    cr.style.left = "200px";
-    counter = 0;
-  }
-
-  cr.style.left = `${counter}00px`;
-  counter++;
   h.style.color = `rgb(${ran}, ${ran2}, ${ran3})`;
 }
 
--- app/app.js
+++ app/app.js
@@ -3,6 +3,14 @@ const app = express();
 var bodyParser = require("body-parser");
 const path = require("path");
 const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const sessions = require('express-session');
+
+
+const oneDay = 1000 * 60 * 60 * 24;
+app.use(sessions({
+  secret: 
+}))
 
 app.use(express.static("D:/Coding/E-Commerce_WebSite/Front"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,6 +20,7 @@ app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join("D:/Coding/E-Commerce_WebSite/Front/index.html"));
+
 });
 
 app.listen(4000, () => {

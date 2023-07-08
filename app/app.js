7fa02540d89ff72785bbfab0f0794e3814757749
Eslamzaid <113968253+Eslamzaid@users.noreply.github.com>
2023-07-08 19:20:50 +0300

    Shoppy

--- app/app.js
+++ app/app.js
@@ -1,26 +1,64 @@
 const express = require("express");
-const app = express();
-var bodyParser = require("body-parser");
-const path = require("path");
+const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
-const sessions = require('express-session');
+const session = require("express-session");
+const app = express();
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
 
-
-const oneDay = 1000 * 60 * 60 * 24;
-app.use(sessions({
-  secret: 
-}))
-
-app.use(express.static("D:/Coding/E-Commerce_WebSite/Front"));
+app.use(
+  session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static("D:/Coding/E-Commerce_WebSite/Front"));
+app.set("view engine", "ejs");
 app.use(morgan("tiny"));
+app.use(cookieParser());
 
-
+var sessions;
 app.get("/", (req, res) => {
-  res.sendFile(path.join("D:/Coding/E-Commerce_WebSite/Front/index.html"));
+  sessions = req.session;
 
+  if (session.userid) {
+    res.send("Oh now our table");
+  } else {
+    res.render("D:/Coding/E-Commerce_WebSite/Front/index.ejs", {
+      situation: "Register now to Order",
+    });
+  }
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile("D:/Coding/E-Commerce_WebSite/Front/about.html");
+});
+
+app.get("/register", (req, res) => {
+  res.render("D:/Coding/E-Commerce_WebSite/Front/register.ejs", {
+    err: req.session.error || "",
+  });
+});
+
+app.post("/register", (req, res) => {
+  console.log(req.body);
+  let name = req.body.name,
+    email = req.body.email,
+    password = req.body.password;
+  if (!name || !email || !password) {
+    req.session.error = "Please fill in all the fields.";
+    res.redirect("/register");
+  } else {
+    // Process the registration logic here
+    // ...
+    // If the registration is successful, you can clear the error message from the session
+    delete session.error;
+    res.redirect("/");
+  }
 });
 
 app.listen(4000, () => {
